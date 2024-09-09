d74022ac7a1609e5d6e7566b818908168c62471b
Gift Mkwara <gift.mkwara@gmail.com>
2024-09-09 14:04:36 +1200

    working on video maker lambda

--- lib/video-maker/local-runner/index.ts
+++ lib/video-maker/local-runner/index.ts
@@ -1,11 +1,13 @@
 import { makeVideo } from '../src'
 import ora from 'ora'
-import { selectFile } from './selectFile'
+import { selectFile } from '@chef-hat/local-runner-utils'
 import { AUDIO_DIRECTORY, IMAGES_DIRECTORY } from './constants'
 import { getOutputPath } from './getOutputPath'
 import figlet from 'figlet'
 
-console.log(figlet.textSync('Local', { font: 'Sub-Zero' }))
+console.log(
+	figlet.textSync('Video Maker', { font: 'Sub-Zero', whitespaceBreak: true }),
+)
 
 const audioPath = await selectFile(
 	AUDIO_DIRECTORY,
@@ -15,7 +17,11 @@ const imagePath = await selectFile(IMAGES_DIRECTORY, 'Please select an image')
 const outputPath = await getOutputPath()
 
 const spinner = ora().start()
-const { errors } = await makeVideo({ audioPath, imagePath, outputPath })
+const { errors } = await makeVideo({
+	audioPath,
+	imagePath,
+	outputPath,
+})
 
 if (errors.length) {
 	spinner.fail(
