a484460ab823ab98f41c4bf1e2cb5434e7a6b763
Gift Mkwara <gift.mkwara@gmail.com>
2024-09-23 10:28:25 +1200

    fixed up the local runner to use new api for resizing

--- apps/image-processor-lambda/local-runner/index.ts
+++ apps/image-processor-lambda/local-runner/index.ts
@@ -2,6 +2,7 @@ import { selectFile, introText } from '@chef-hat/local-runner-utils'
 import { INPUTS_DIRECTORY, OUTPUTS_DIRECTORY } from './constants'
 import { resizeImage } from '../src/resizeImage'
 import path from 'node:path'
+import fs from 'node:fs'
 import ora from 'ora'
 import { SixteenByNine } from '../src/AspectRatio'
 import { isSuccess } from '@chef-hat/ts-result'
@@ -9,12 +10,15 @@ import { isSuccess } from '@chef-hat/ts-result'
 introText('Image Processor')
 
 const imagePath = await selectFile(INPUTS_DIRECTORY, 'Please select an image')
+const imageBuffer = Buffer.concat(
+	await fs.createReadStream(imagePath).toArray(),
+)
 const outputPath = path.join(
 	OUTPUTS_DIRECTORY,
 	`${new Date().toLocaleTimeString()}.png`,
 )
 const spinner = ora().start()
-const result = await resizeImage(imagePath, SixteenByNine, outputPath)
+const result = await resizeImage(imageBuffer, SixteenByNine, outputPath)
 
 if (isSuccess(result)) {
 	spinner.succeed()
