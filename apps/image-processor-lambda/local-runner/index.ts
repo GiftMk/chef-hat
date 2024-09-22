be5814ad5935fa4b95c6df38be5f9fbfbd6e6827
Gift Mkwara <gift.mkwara@gmail.com>
2024-09-23 10:03:49 +1200

    using local time string for the test output name

--- apps/image-processor-lambda/local-runner/index.ts
+++ apps/image-processor-lambda/local-runner/index.ts
@@ -11,7 +11,7 @@ introText('Image Processor')
 const imagePath = await selectFile(INPUTS_DIRECTORY, 'Please select an image')
 const outputPath = path.join(
 	OUTPUTS_DIRECTORY,
-	`${new Date().toUTCString()}.png`,
+	`${new Date().toLocaleTimeString()}.png`,
 )
 const spinner = ora().start()
 const result = await resizeImage(imagePath, SixteenByNine, outputPath)
