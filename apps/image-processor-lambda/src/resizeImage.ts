1ef8c9f5e4638c325e2c4afc2a6750d4cf1a5385
Gift Mkwara <gift.mkwara@gmail.com>
2024-10-04 21:06:07 +1300

    wrapped resizeImage implementation in a try catch

--- apps/image-processor-lambda/src/resizeImage.ts
+++ apps/image-processor-lambda/src/resizeImage.ts
@@ -10,10 +10,20 @@ export const resizeImage = async (
 ): Promise<Result> => {
 	const { width, height } = aspectRatio
 	const outputStream = fs.createWriteStream(outputPath)
-	sharp(imagePath).resize(width, height).png().pipe(outputStream, { end: true })
+	try {
+		sharp(imagePath)
+			.resize(width, height)
+			.png()
+			.pipe(outputStream, { end: true })
 
-	return await new Promise<Result>((resolve, reject) => {
-		outputStream.on('close', () => resolve(emptySuccess()))
-		outputStream.on('error', (e) => reject(failure(e.message)))
-	})
+		return await new Promise<Result>((resolve, reject) => {
+			outputStream.on('close', () => resolve(emptySuccess()))
+			outputStream.on('error', (e) => reject(failure(e.message)))
+		})
+	} catch (e) {
+		if (e instanceof Error) {
+			return failure(e.message)
+		}
+		return failure(`An unknown error occurred when resizing image ${imagePath}`)
+	}
 }
