4124d11f42587a8be2fd262f179bae4a2b5fbcd3
Gift Mkwara <gift.mkwara@gmail.com>
2024-10-06 12:34:45 +1300

    set font size for intro text to 80

--- lib/local-runner-utils/src/introText.ts
+++ lib/local-runner-utils/src/introText.ts
@@ -3,6 +3,7 @@ import figlet from 'figlet'
 export const introText = (text: string) => {
 	console.log(
 		figlet.textSync(text, {
+			width: 80,
 			font: 'Sub-Zero',
 			whitespaceBreak: true,
 		}),
