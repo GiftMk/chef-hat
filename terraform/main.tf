a7c8fa046b21403cc13e5dd77d3216e819df31ec
Gift Mkwara <gift.mkwara@gmail.com>
2024-10-07 16:16:56 +1300

    wrote initial terraform

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "cheff-hat-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
