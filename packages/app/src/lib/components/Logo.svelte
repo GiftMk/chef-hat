abf643609b3bf733a0db085ed2d2a8e230bb6359
Gift Mkwara <gift.mkwara@gmail.com>
2024-10-28 12:33:25 +1300

    added svelte 5 app

---
+++ packages/app/src/lib/components/Logo.svelte
@@ -0,0 +1,1 @@
+<h1>Cheff Hat Video Maker</h1>
---
+++ packages/app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import '../app.css'
+import Logo from '$lib/components/Logo.svelte'
+let { children } = $props()
+</script>
+
+<main class="w-full h-full max-w-5xl flex p-16 justify-center m-auto">
+	<Logo />
+{@render children()}
+</main>
